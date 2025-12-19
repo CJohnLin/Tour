4d738378f97d7b626c7f199a00e143d280ad1e49
CJohnLin <jhlin94228@gmail.com>
2025-12-20 01:19:06 +0800

    Initial travel app

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./App.vue",
+    "./main.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
